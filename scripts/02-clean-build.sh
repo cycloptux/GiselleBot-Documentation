9e21668aef85600212fae60503f758813c9a020e
cycloptux <ale.impe@gmail.com>
2019-06-17 14:53:02 +0200

    Final test commit, auto-build should work from now on.

---
+++ scripts/02-clean-build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Clean the previous build
+rm -rf /var/www/docs.cycloptux.com/gisellebot/public_html/*
--- scripts/create-folder.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Create website folder if it doesn't exist
-mkdir -p /var/www/docs.cycloptux.com/gisellebot
