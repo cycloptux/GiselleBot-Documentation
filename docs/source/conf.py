5f39b18be7329f372c9f686bdcb0766fce7b576a
cycloptux <ale.impe@gmail.com>
2019-06-28 17:56:52 +0200

    Updated version.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, cycloptux'
 author = 'cycloptux'
 
 # The short X.Y version
-version = '2.0'
+version = '3.0'
 # The full version, including alpha/beta/rc tags
-release = '2.0.0-alpha'
+release = '3.0.0-beta'
 
 # Substitutions
 rst_prolog = '''
