738a642f936b970bc333c208d8cccc174e81b433
cycloptux <ale.impe@gmail.com>
2019-12-16 10:02:04 +0100

    Replaced the invite with its short URL.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ release = '3.2.0'
 # Substitutions
 rst_prolog = '''
 .. |bot_name| replace:: GiselleBot
-.. |bot_invite| replace:: https://discordapp.com/oauth2/authorize?client_id=356831787445387285&permissions=301329654&scope=bot
+.. |bot_invite| replace:: https://gisl.eu/invite
 .. |bot_prefix| replace:: !
 .. |bot_prefix_name| replace:: an exclamation point
 '''
