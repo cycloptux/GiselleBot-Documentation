d68bb7d7e9fd39b11bea2b3071b6dd97bc276a7b
cycloptux <ale.impe@gmail.com>
2021-04-23 22:52:01 +0200

    Added config for sphinx_markdown_parser.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -198,3 +198,40 @@ epub_exclude_files = ['search.html']
 # Customizing the URL scheme
 # sitemap_url_scheme = "{lang}{version}{link}"
 sitemap_url_scheme = "{lang}{link}"
+
+# for MarkdownParser
+from sphinx_markdown_parser.parser import MarkdownParser
+
+def setup(app):
+    app.add_source_suffix('.md', 'markdown')
+    app.add_source_parser(MarkdownParser)
+    app.add_config_value('markdown_parser_config', {
+        'auto_toc_tree_section': 'Content',
+        'enable_auto_doc_ref': True,
+        'enable_auto_toc_tree': True,
+        'enable_eval_rst': True,
+        'extensions': [
+            'extra',
+            'nl2br',
+            'sane_lists',
+            'smarty',
+            'toc',
+            'wikilinks',
+            'pymdownx.arithmatex',
+        ],
+    }, True)
+
+# for CommonMarkParser
+# from recommonmark.parser import CommonMarkParser
+# 
+# def setup(app):
+#     app.add_source_suffix('.md', 'markdown')
+#     app.add_source_parser(CommonMarkParser)
+#     app.add_config_value('markdown_parser_config', {
+#         'auto_toc_tree_section': 'Content',
+#         'enable_auto_doc_ref': True,
+#         'enable_auto_toc_tree': True,
+#         'enable_eval_rst': True,
+#         'enable_inline_math': True,
+#         'enable_math': True,
+#     }, True)
